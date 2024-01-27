eb222331c7553a55605f9c347497d8ce18b45a22
christianlepio <rylepio79@gmail.com>
2024-01-28 05:08:28 +0800

    create login component, main layout, and 404 page

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,14 +1,25 @@
 import { Routes, Route } from "react-router-dom"
+// components
 import DashLayout from "./DashLayout"
 import Dashboard from "./Dashboard"
+import MainLayout from "./MainLayout"
+import Login from "./Login"
+import PageNotFound from "./PageNotFound"
 
 const AppRoutes = () => {
     return (
         <Routes>
-            {/* dashboard parent route */}
-            <Route path="dash" element={<DashLayout />}>
-                <Route index element={<Dashboard />} />
-            </Route> 
+            {/* parent route */}
+            <Route path="/" element={<MainLayout />} >
+                <Route index element={<Login />} />
+
+                {/* dashboard parent route */}
+                <Route path="dash" element={<DashLayout />} >
+                    <Route index element={<Dashboard />} />
+                </Route> 
+                {/* catch all missing page */}
+                <Route path="*" element={<PageNotFound />} />
+            </Route>
         </Routes>
     )
 }
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,10 @@
+
+const PageNotFound = () => {
+    return (
+        <main>
+            <h1 className="text-center h1">404 Page not found!</h1>
+        </main>
+    )
+}
+
+export default PageNotFound

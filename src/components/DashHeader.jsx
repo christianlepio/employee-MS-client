b5216338c820a755e61abf3f588f48176e4636ab
christianlepio <rylepio79@gmail.com>
2024-01-28 15:49:19 +0800

    update bg-color to light blue

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -1,7 +1,7 @@
 
 const DashHeader = () => {
     return (
-        <header className="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+        <header className="navbar sticky-top bg-info flex-md-nowrap p-0 shadow">
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company Name</a>
             <ul className="navbar-nav flex-row d-md-none">
                 <li className="nav-item text-nowrap">

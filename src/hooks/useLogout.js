ff0aba94da123bcaf8b6d57842edf6670661a3ab
christianlepio <rylepio79@gmail.com>
2024-01-29 02:28:26 +0800

    create custom hook for logout

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,22 @@
+import axios from "../api/axios"
+import useAuth from "./useAuth"
+
+const useLogout = () => {
+    const { setAuth } = useAuth()
+
+    const logout = async () => {
+        setAuth({})
+
+        try {
+            const response = await axios.get('/auth/logout', {
+                withCredentials: true
+            })
+        } catch (err) {
+            console.error('Logout error: ', err)
+        }
+    }
+
+    return logout
+}
+
+export default useLogout

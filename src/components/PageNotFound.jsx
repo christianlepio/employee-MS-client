88f2ad180d6bec8a8fc169c108a6784d3b04862a
christianlepio <rylepio79@gmail.com>
2024-01-30 05:51:54 +0800

    modify style page not found

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,7 +1,7 @@
 
 const PageNotFound = () => {
     return (
-        <main>
+        <main className="d-flex main-login-regi align-items-center justify-content-center">
             <h1 className="text-center h1">404 Page not found!</h1>
         </main>
     )

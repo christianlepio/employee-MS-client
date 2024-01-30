bd8ea685ec7bce29064a9c7d554d2d696e631d6f
christianlepio <rylepio79@gmail.com>
2024-01-31 06:52:28 +0800

    display username and user icon

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -1,10 +1,23 @@
+import useAuth from "../hooks/useAuth"
+import { jwtDecode } from 'jwt-decode'
 
 const DashHeader = () => {
+    const { auth } = useAuth()
+
+    //decode the encrypted accesstoken using jwt decode
+    const decoded = auth?.accessToken 
+        ? jwtDecode(auth.accessToken)
+        : undefined
+
     return (
         <header className="navbar sticky-top bg-info flex-md-nowrap p-0 shadow">
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company Name</a>
-            <ul className="navbar-nav flex-row d-md-none">
-                <li className="nav-item text-nowrap">
+            <ul className="navbar-nav flex-row">
+                <div className="d-flex me-sm-4 text-secondary">
+                    <p className="mt-2 me-2">{`${decoded?.UserInfo?.firstName} ${decoded?.UserInfo?.lastName}`}</p>
+                    <i className="bi bi-person-circle fs-3"></i>
+                </div>
+                <li className="nav-item text-nowrap d-md-none">
                     <button className="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                         <i className="bi bi-list"></i>
                     </button>

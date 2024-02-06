2a0f9478a8dbbde27d2a9faf862fabeb80d823c3
christianlepio <rylepio79@gmail.com>
2024-02-06 19:02:59 +0800

    create edit employee modal component

---
+++ src/components/EditEmpModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import useAuth from '../hooks/useAuth'
+import useAxiosPrivate from '../hooks/useAxiosPrivate'
+
+const EditEmpModal = ({ mdlId, prEmployee }) => {
+    return (
+        <div>EditEmpModal</div>
+    )
+}
+
+export default EditEmpModal
